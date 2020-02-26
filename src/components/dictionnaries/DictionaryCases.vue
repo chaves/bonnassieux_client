8bd3dc888fa300e2f4993e4445a4f7df0258b30c
Bruno Chaves <chabruno@gmail.com>
2020-02-26 11:45:02 +0100

    factorisations suite

--- src/components/dictionnaries/DictionaryCases.vue
+++ src/components/dictionnaries/DictionaryCases.vue
@@ -1,10 +1,27 @@
 <template>
-  <v-row>
-    <v-col cols="12" md="6">
-      <div>Cases</div>
-      {{matters}}
-    </v-col>
-  </v-row>
+  <v-card>
+    <v-card-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table 
+      :headers="headers" 
+      :items="matters" 
+      :search="search" 
+      class="elevation-1"
+      :footer-props="{
+        itemsPerPageOptions: [50,100,200,-1]
+      }"
+      dense
+    >
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
@@ -12,7 +29,14 @@ export default {
   data: () => ({
     matters: [],
   }),
-
+  computed: {
+    headers() {
+      return [
+        { text: "Total", sortable: false, value: "sources_count" },
+        { text: "Name", sortable: false, value: "name" }
+      ]
+    }
+  },
   mounted() {
     window.axios
       .get('matters/counts')
