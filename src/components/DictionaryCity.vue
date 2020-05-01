d1c31621925ea9b8fa0ffcf3470f87122f1ef852
Bruno Chaves <chabruno@gmail.com>
2020-05-01 17:27:35 +0200

    get sources related to dictionaries

--- src/components/DictionaryCity.vue
+++ src/components/DictionaryCity.vue
@@ -22,11 +22,37 @@
         itemsPerPageOptions: [200,500,1000,-1]
       }"
       dense
+      @click:row="showSources"
     >
     <template v-slot:item.remove="{ item }">
       <v-icon color="red" @click="remove(item)">mdi-delete</v-icon>
     </template>
     </v-data-table>
+
+    <v-row justify="center">
+      <v-dialog v-model="dialog" scrollable>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+        </template>
+        <v-card>
+          <v-card-title><strong>{{selectedDictItem}}</strong>...<a @click="dialog = false">close</a></v-card-title>
+          <v-card-subtitle><span class="dict_term">{{url}}</span></v-card-subtitle>
+          <v-divider></v-divider>
+          <v-card-text style="height: 600px;">
+            <v-list-item v-for="source in sources" :key="source.id">
+              <v-list-item-content>
+                <v-list-item-title>{{source.id}} - <strong>{{source.date}}</strong><br><span v-html="highlightItem(source.source)"></span></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="red" text @click="dialog = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+
   </v-card>
 </template>
 
@@ -36,8 +62,11 @@ export default {
   data: function () {
     return {
       datas: [],
+      sources: [],
       search: '',
       item: '',
+      dialog: false,
+      selectedDictItem: '',
       rules: {
       required: value => !!value || 'Required.'
       }
@@ -78,7 +107,37 @@ export default {
       .catch(err => {
         console.log(err)
       })
+    },
+    getSources(id) {
+      window.axios
+              .get('sources/dictionaries/' + this.url + '/' + id)
+              .then(response => this.sources = response.data);
+    },
+    showSources(item) {
+      this.selectedDictItem = item.name;
+      this.dialog = true;
+      this.sources = this.getSources(item.id);
+    },
+    highlightItem(source) {
+      const regex = RegExp('(\\b'+ this.selectedDictItem +'[s]?\\b)', "igm");
+      source = source.replace(regex, match => {
+        return (
+                '<span style="background-color: #FFFF00">' + match + '</span>'
+        );
+      })
+      return source
     }
   }
 }
 </script>
+
+<style>
+
+tr:hover {
+  cursor: pointer;
+}
+.dict_term {
+  text-transform: capitalize;
+}
+
+</style>
---
+++ src/components/parts/highlightSourceDictionary.vue
@@ -0,0 +1,80 @@
+<template>
+  <span v-html="highlight(im)"></span>
+</template>
+
+<script>
+export default {
+  props: ["source", "industries", "cities", "regions", "persons", "search"],
+  methods: {
+    highlight: function(source, industries, cities, regions, persons, search) {
+
+      industries.map(function(value) {
+        const regex = RegExp('(\\b'+ value.name +'[s]?\\b)', "igm");
+        source = source.replace(
+          regex,
+          match => {
+            return (
+              '<span style="background-color: #FFFF00">' + match + '</span>'
+            );
+          }
+        );
+      });
+
+      cities.map(function(value) {
+        const regex = RegExp('(\\b'+ value.name +'[s]?\\b)', "igm");
+        source = source.replace(
+          regex,
+          match => {
+            return (
+              '<span style="background-color: #ffcccc">' + match + '</span>'
+            );
+          }
+        );
+      });
+
+      regions.map(function(value) {
+        const regex = RegExp('(\\b'+ value.name +'[s]?\\b)', "igm");
+        source = source.replace(
+          regex,
+          match => {
+            return (
+              '<span style="background-color: #99ffbb">' + match + '</span>'
+            );
+          }
+        );
+      });
+
+      persons.map(function(value) {
+        const regex = RegExp('(\\b'+ value.name +'[s]?\\b)', "igm");
+        source = source.replace(
+          regex,
+          match => {
+            return (
+              '<span style="background-color: #ccccff">' + match + '</span>'
+            );
+          }
+        );
+      });
+
+      if(search.length >= 2) {
+        const regex = RegExp('(\\b'+ search + ')', "igm");
+        source = source.replace(
+          regex,
+          match => {
+            return (
+              '<span style="background-color: #000; color: #fff">' + match + '</span>'
+            );
+          }
+        );
+      }
+
+
+      return source;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
